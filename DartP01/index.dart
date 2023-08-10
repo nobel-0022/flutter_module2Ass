b8fbac5ee5e7f5ed46d3dba534d6deb41d640966
nobel-0022 <83616937+nobel-0022@users.noreply.github.com>
2023-08-10 20:38:25 +0600

    Add files via upload

---
+++ DartP01/index.dart
@@ -0,0 +1,21 @@
+void main(){
+   int subject = 880;
+
+   if(subject >= 90 && subject <= 100){
+     print("pass A+");
+   }else if(subject >= 80 && subject <=90){
+     print("pass A");
+   }else if(subject >= 70 && subject <= 80){
+     print("pass A-");
+   }else if (subject >= 60 && subject <= 70){
+     print("Sorry You are Fail");
+   }else{
+     print("Invalid number");
+   }
+}
+
+
+
+
+
+
